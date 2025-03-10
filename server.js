8f818acf8b0c31a75a0d81a867b2365f2c4de6e6
Luigi <pesaroluigi5@gmail.com>
2025-03-10 20:50:41 +0100

    fix porta

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Configura il database PostgreSQL
 const pool = new Pool({
