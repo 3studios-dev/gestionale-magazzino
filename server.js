54455537dbaf4dba7b8aa9a965de4e4aeaa823b8
Luigi <pesaroluigi5@gmail.com>
2025-03-11 01:40:54 +0100

    change

--- server.js
+++ server.js
@@ -1,17 +1,91 @@
 const express = require("express");
+const { main } = require("./models/index");
+const productRoute = require("./router/product");
+const storeRoute = require("./router/store");
+const purchaseRoute = require("./router/purchase");
+const salesRoute = require("./router/sales");
 const cors = require("cors");
-require("dotenv").config();
+const User = require("./models/users");
+const Product = require("./models/Product");
 
-const articoliRoutes = require("./routes/articoliRoutes");
-const operazioniRoutes = require("./routes/operazioniRoutes");
 
 const app = express();
-const PORT = process.env.PORT || 8080;
-
-app.use(cors());
+const PORT = 4000;
+main();
 app.use(express.json());
+app.use(cors());
 
-app.use("/articoli", articoliRoutes);
-app.use("/operazioni", operazioniRoutes);
+// Store API
+app.use("/api/store", storeRoute);
 
-app.listen(PORT, () => console.log(`✅ Server avviato su http://localhost:${PORT}`));
+// Products API
+app.use("/api/product", productRoute);
+
+// Purchase API
+app.use("/api/purchase", purchaseRoute);
+
+// Sales API
+app.use("/api/sales", salesRoute);
+
+// ------------- Signin --------------
+let userAuthCheck;
+app.post("/api/login", async (req, res) => {
+  console.log(req.body);
+  // res.send("hi");
+  try {
+    const user = await User.findOne({
+      email: req.body.email,
+      password: req.body.password,
+    });
+    console.log("USER: ", user);
+    if (user) {
+      res.send(user);
+      userAuthCheck = user;
+    } else {
+      res.status(401).send("Invalid Credentials");
+      userAuthCheck = null;
+    }
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+});
+
+// Getting User Details of login user
+app.get("/api/login", (req, res) => {
+  res.send(userAuthCheck);
+});
+// ------------------------------------
+
+// Registration API
+app.post("/api/register", (req, res) => {
+  let registerUser = new User({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    password: req.body.password,
+    phoneNumber: req.body.phoneNumber,
+    imageUrl: req.body.imageUrl,
+  });
+
+  registerUser
+    .save()
+    .then((result) => {
+      res.status(200).send(result);
+      alert("Signup Successfull");
+    })
+    .catch((err) => console.log("Signup: ", err));
+  console.log("request: ", req.body);
+});
+
+
+app.get("/testget", async (req,res)=>{
+  const result = await Product.findOne({ _id: '6429979b2e5434138eda1564'})
+  res.json(result)
+
+})
+
+// Here we are listening to the server
+app.listen(PORT, () => {
+  console.log("I am live again");
+});
